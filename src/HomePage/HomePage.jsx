c9746f62c54c0e46db96ab678641ab268cd34029
dulyup <31580159+dulyup@users.noreply.github.com>
2019-07-24 17:08:49 -0700

    reformat home and about page

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -7,16 +7,23 @@ import sharedStyle from "../Shared.module.css";
 const HomePage = () => {
   return (
     <div className={sharedStyle.homeAboutPage}>
+      <hr/>
+      <p className={sharedStyle.firstPar}>
+        <h3>"</h3>
+        Seattle Technological University is devoted to excellence in teaching, learning, and research, and to developing leaders in many disciplines who make a difference globally.
+        <h3>"</h3>
+      </p>
       <img src={"images/graduation.jpeg"} alt={"Students"} className={sharedStyle.image}/>
       <section className={sharedStyle.description}>
-        <h1>S</h1>
-        eattle Technological University is devoted to excellence in teaching,
-        learning, and research, and to developing leaders in many disciplines who
-        make a difference globally. We have an enrollment of over 20,000 degree
-        candidates, including undergraduate, graduate, and professional students.
-        We have more than 360,000 alumni around the world.
         <p>
-          Our faculty are engaged with teaching and research to push the boundaries
+          <h3>W</h3>
+          e have an enrollment of over 20,000 degree
+          candidates, including undergraduate, graduate, and professional students.
+          We have more than 360,000 alumni around the world.
+        </p>
+        <p>
+          <h3>O</h3>
+          ur faculty are engaged with teaching and research to push the boundaries
           of human knowledge. For students who are excited to investigate the
           biggest issues of the 21st century, we offer an unparalleled student
           experience and a generous financial aid program, with over $160 million
@@ -24,7 +31,8 @@ const HomePage = () => {
           degree-granting Schools, offering a truly global education.
         </p>
         <p>
-          Established in 1936, Seattle Technological University is the oldest
+          <h3>E</h3>
+          stablished in 1936, Seattle Technological University is the oldest
           institution of higher education in Seattle.
         </p>
       </section>
