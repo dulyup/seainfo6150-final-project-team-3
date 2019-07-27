cbb43aaa8b774174dc0711f238727f72555615d8
dulyup <31580159+dulyup@users.noreply.github.com>
2019-07-27 02:32:22 -0700

    fix level color and html

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -9,21 +9,20 @@ const HomePage = () => {
     <div className={sharedStyle.homeAboutPage}>
       <hr/>
       <p className={sharedStyle.firstPar}>
-        <h3>"</h3>
+        <span className={sharedStyle.initial}>"</span>
         Seattle Technological University is devoted to excellence in teaching, learning, and research, and to developing leaders in many disciplines who make a difference globally.
-        <h3>"</h3>
+        <span className={sharedStyle.initial}>"</span>
       </p>
       <img src={"images/graduation.jpeg"} alt={"Students"} className={sharedStyle.image}/>
       <section className={sharedStyle.description}>
         <p>
-          <h3>W</h3>
+          <span className={sharedStyle.initial}>W</span>
           e have an enrollment of over 20,000 degree
           candidates, including undergraduate, graduate, and professional students.
           We have more than 360,000 alumni around the world.
         </p>
         <p>
-          <h3>O</h3>
-          ur faculty are engaged with teaching and research to push the boundaries
+          Our faculty are engaged with teaching and research to push the boundaries
           of human knowledge. For students who are excited to investigate the
           biggest issues of the 21st century, we offer an unparalleled student
           experience and a generous financial aid program, with over $160 million
@@ -31,8 +30,7 @@ const HomePage = () => {
           degree-granting Schools, offering a truly global education.
         </p>
         <p>
-          <h3>E</h3>
-          stablished in 1936, Seattle Technological University is the oldest
+          Established in 1936, Seattle Technological University is the oldest
           institution of higher education in Seattle.
         </p>
       </section>
